54504bb2ceca22273bcd71d18cead2d0e4c4ae1e
G9NCUE <159912605+G9NCUE@users.noreply.github.com>
2026-09-16 20:11:54 +0800

    Vite 8: optimizeDeps.rolldownOptions instead of the deprecated esbuildOptions

--- vite.config.js
+++ vite.config.js
@@ -43,7 +43,7 @@ export default defineConfig({
       '@ledgerhq/signer-utils',
       'rxjs'
     ],
-    esbuildOptions: { define: { global: 'globalThis' } }
+    rolldownOptions: { define: { global: 'globalThis' } }
   },
   server: {
     proxy: { '/api': 'http://127.0.0.1:8787' }
